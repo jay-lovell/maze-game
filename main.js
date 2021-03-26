d686218c6c447f13df6dbd20f367a36af581d001
LOVELL <jay.lovell2@admiralgroup.co.uk>
2021-03-26 10:28:35 +0000

    Styled, maze complete, player can move

--- main.js
+++ main.js
@@ -1,13 +1,56 @@
-// create maze area
+const grid = document.getElementById("maze-area");
+
+const player = document.getElementById("player");
+
+const domRect = grid.getBoundingClientRect();
+
+
+let leftPos = 262;
+let topPos = 425;
+
+
+
+startingPos = document.querySelector
 
 
 
 
-// draw maze
+// player
+
+function getGridElementPosition(index) {
+
+}
 
 
 
-// player marker and movement
+
+
+//  movement
+
+document.addEventListener("keydown", event => {
+
+  switch (event.keyCode) {
+
+    case 39: // move right
+      leftPos += 3;
+      player.style.left = leftPos + 'px';
+      return;   
+    case 37: // move left
+      leftPos -= 3;
+      player.style.left = leftPos + 'px';
+      return;
+    case 40: // move down
+      topPos += 3;
+      player.style.top = topPos + 'px';
+      return;
+    case 38: // move up
+      topPos -= 3;
+      player.style.top = topPos + 'px';
+      return;
+  }
+
+
+});
 
 
 
